5358202acb5a6de86a2e2af377933ec7b7af8d49
Saurav Sharma <saurav@codenomad.net>
2024-09-06 13:14:18 +0530

    changes

--- src/common/Header.js
+++ src/common/Header.js
@@ -25,7 +25,6 @@ const Header = ({ className }) => {
             setIsVisible(false); // Close the mobile menu after click
         }
     };
-
     return (
         <div className={`fixed top-0 z-10 left-0 right-0 w-full bg-[#32314852] backdrop-blur-lg 2xl:px-0 xl:px-0 lg:px-0 md:px-16 sm:px-8 s:px-8`}>
             <div className={`${className}`}>
@@ -37,22 +36,22 @@ const Header = ({ className }) => {
                         <img src={menu} onClick={openDiv} alt='menu' />
                     </div>
                     <div className='justify-between text-white self-center 2xl:flex xl:flex lg:flex md:flex sm:hidden s:hidden'>
-                        <a onClick={() => scrollToSection('plan')} className='ml-5 self-center 4xl:text-[22px] 3xl:text-[19px] 2xl:text-[16px] xl:text-[16px] lg:text-[16px] md:text-[16px] sm:text-[16px] s:text-[16px] cursor-pointer'>Our Plans</a>
+                        {/* <a onClick={() => scrollToSection('plan')} className='ml-5 self-center 4xl:text-[22px] 3xl:text-[19px] 2xl:text-[16px] xl:text-[16px] lg:text-[16px] md:text-[16px] sm:text-[16px] s:text-[16px] cursor-pointer'>Our Plans</a>
                         <a onClick={() => scrollToSection('about-us')} className='ml-5 self-center 4xl:text-[22px] 3xl:text-[19px] 2xl:text-[16px] xl:text-[16px] lg:text-[16px] md:text-[16px] sm:text-[16px] s:text-[16px] cursor-pointer'> About Us</a>
-                        <a onClick={() => scrollToSection('why-us')} className='ml-5 self-center 4xl:text-[22px] 3xl:text-[19px] 2xl:text-[16px] xl:text-[16px] lg:text-[16px] md:text-[16px] sm:text-[16px] s:text-[16px] cursor-pointer'>Why Us</a>
+                        <a onClick={() => scrollToSection('why-us')} className='ml-5 self-center 4xl:text-[22px] 3xl:text-[19px] 2xl:text-[16px] xl:text-[16px] lg:text-[16px] md:text-[16px] sm:text-[16px] s:text-[16px] cursor-pointer'>Why Us</a> */}
                         <a onClick={() => scrollToSection('contact-us')} className='ml-5 self-center 4xl:text-[22px] 3xl:text-[19px] 2xl:text-[16px] xl:text-[16px] lg:text-[16px] md:text-[16px] sm:text-[16px] s:text-[16px] cursor-pointer'>Contact Us</a>
-                        <a href="https://app.getcoverusa.com/" target='_blank' className='text-[#323148] 4xl:text-[20px] 3xl:text-[19px] 2xl:text-[12px] xl:text-[12px] lg:text-[12px] md:text-[12px] sm:text-[12px] s:text-[12px] font-semibold bg-white rounded-lg p-3 ml-5'>LOGIN</a>
+                        <a href="https://app.getcover.com/" target='_blank' className='text-[#323148] 4xl:text-[20px] 3xl:text-[19px] 2xl:text-[12px] xl:text-[12px] lg:text-[12px] md:text-[12px] sm:text-[12px] s:text-[12px] font-semibold bg-white rounded-lg p-3 ml-5'>LOGIN</a>
                     </div>
 
                     {isVisible && (
                         <div className='fixed top-0 left-0 w-full h-screen bg-[#323148fa] backdrop-blur-2xl'>
                             <img src={cross} onClick={openDiv} className='ml-5 mt-5' alt='cross' />
                             <div className='mt-28  w-full  mx-auto'>
-                                <p className='text-center my-5 text-white cursor-pointer' onClick={() => scrollToSection('plan')}>Our Plans</p>
+                                {/* <p className='text-center my-5 text-white cursor-pointer' onClick={() => scrollToSection('plan')}>Our Plans</p>
                                 <p className='text-center mb-5 text-white cursor-pointer' onClick={() => scrollToSection('about-us')}> About Us</p>
-                                <p className='text-center mb-5 text-white cursor-pointer' onClick={() => scrollToSection('why-us')}>Why Us</p>
+                                <p className='text-center mb-5 text-white cursor-pointer' onClick={() => scrollToSection('why-us')}>Why Us</p> */}
                                 <p className='text-center mb-5 text-white cursor-pointer' onClick={() => scrollToSection('contact-us')}>Contact Us</p>
-                                <p className='text-center my-5'><a href="https://app.getcoverusa.com/" target='_blank' className='text-[#323148] text-sm font-semibold bg-white rounded-lg p-3'>LOGIN</a></p>
+                                <p className='text-center my-5'><a href="https://app.getcover.com/" target='_blank' className='text-[#323148] text-sm font-semibold bg-white rounded-lg p-3'>LOGIN</a></p>
                             </div>
                         </div>
                     )}
